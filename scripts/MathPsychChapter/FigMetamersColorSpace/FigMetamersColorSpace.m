5def26bbe2144d23f053be44a21b205442fc50f6
David Brainard <brainard@psych.upenn.edu>
2022-05-15 09:42:49 -0400

    Edis

--- scripts/MathPsychChapter/FigMetamersColorSpace/FigMetamersColorSpace.m
+++ scripts/MathPsychChapter/FigMetamersColorSpace/FigMetamersColorSpace.m
@@ -41,6 +41,7 @@ B_metamer = MakeGaussBasis(wls,[430 520 650],8*[120 120 120]);
 % Get a spectrum
 spd1 = GenerateBlackBody(4000,wls);
 spd1 = spd1/max(spd1);
+spd1 =spd1/2;
 XYZ1 = T*spd1;
 metamer1 = B_metamer*inv(T*B_metamer)*XYZ1;
 XYZ1Check = T*metamer1;
@@ -54,10 +55,10 @@ figParams.xLimLow = 380;
 figParams.xLimHigh = 720;
 figParams.xTicks = [400 500 600 700];
 figParams.xTickLabels = {'400' '500' '600' '700'};
-figParams.yLimLow = -0.01;
-figParams.yLimHigh = 0.76;
-figParams.yTicks = [0.0 0.25 0.5 0.75];
-figParams.yTickLabels = {' 0.00 ' ' 0.25 ' ' 0.50 ' ' 0.75 '};
+figParams.yLimLow = 0.0;
+figParams.yLimHigh = 1.5;
+figParams.yTicks = [0.0 0.5 1 1.5];
+figParams.yTickLabels = {' 0.00 ' ' 0.50 ' ' 1.00 ' ' 1.50 '};
 theFig1 = figure; clf; hold on
 set(gcf,'Position',[100 100 figParams.sqSize figParams.sqSize]);
 set(gca,'FontName',figParams.fontName,'FontSize',figParams.axisFontSize,'LineWidth',figParams.axisLineWidth);
@@ -67,10 +68,10 @@ xlim([figParams.xLimLow figParams.xLimHigh]);
 set(gca,'XTick',figParams.xTicks);
 set(gca,'XTickLabel',figParams.xTickLabels);
 xlabel('Wavelength (nm)','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
-%ylim([figParams.yLimLow figParams.yLimHigh]);
+ylim([figParams.yLimLow figParams.yLimHigh]);
 ylabel('Power (arb units)','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
-%set(gca,'YTick',figParams.yTicks);
-%set(gca,'YTickLabel',figParams.yTickLabels);
+set(gca,'YTick',figParams.yTicks);
+set(gca,'YTickLabel',figParams.yTickLabels);
 %legend({' L cones ' ' M cones ' ' S cones '},'Location','NorthEast','FontSize',figParams.legendFontSize);
 axis('square');
 %title('Cone Fundamentals (2°)')
@@ -93,10 +94,10 @@ figParams.xLimLow = 380;
 figParams.xLimHigh = 720;
 figParams.xTicks = [400 500 600 700];
 figParams.xTickLabels = {'400' '500' '600' '700'};
-figParams.yLimLow = -0.01;
-figParams.yLimHigh = 0.76;
-figParams.yTicks = [0.0 0.25 0.5 0.75];
-figParams.yTickLabels = {' 0.00 ' ' 0.25 ' ' 0.50 ' ' 0.75 '};
+figParams.yLimLow = 0;
+figParams.yLimHigh = 1.5;
+figParams.yTicks = [0.0 0.5 1 1.5];
+figParams.yTickLabels = {' 0.00 ' ' 0.50 ' ' 1.00 ' ' 1.50 '};
 theFig2 = figure; clf; hold on
 set(gcf,'Position',[100 100 figParams.sqSize figParams.sqSize]);
 set(gca,'FontName',figParams.fontName,'FontSize',figParams.axisFontSize,'LineWidth',figParams.axisLineWidth);
@@ -106,15 +107,45 @@ xlim([figParams.xLimLow figParams.xLimHigh]);
 set(gca,'XTick',figParams.xTicks);
 set(gca,'XTickLabel',figParams.xTickLabels);
 xlabel('Wavelength (nm)','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
-%ylim([figParams.yLimLow figParams.yLimHigh]);
+ylim([figParams.yLimLow figParams.yLimHigh]);
 ylabel('Power (arb units)','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
-%set(gca,'YTick',figParams.yTicks);
-%set(gca,'YTickLabel',figParams.yTickLabels);
+set(gca,'YTick',figParams.yTicks);
+set(gca,'YTickLabel',figParams.yTickLabels);
 %legend({' L cones ' ' M cones ' ' S cones '},'Location','NorthEast','FontSize',figParams.legendFontSize);
 axis('square');
 %title('Cone Fundamentals (2°)')
 %set(gca,'XMinorTick','on');
 % FigureSave(fullfile(pwd,[mfilename '_' figParams.figName]),theFig,figParams.figType);
+
+% Make KXYZ figure 3
+figParams.figName = 'FigXYZ';
+figParams.xLimLow = 380;
+figParams.xLimHigh = 720;
+figParams.xTicks = [400 500 600 700];
+figParams.xTickLabels = {'400' '500' '600' '700'};
+figParams.yLimLow = 0;
+figParams.yLimHigh = 1;
+figParams.yTicks = [0.0 0.2 0.4 0.6 0.8 1.0];
+figParams.yTickLabels = {' 0 ' ' 50 ' ' 100 '};
+theFig2 = figure; clf; hold on
+set(gcf,'Position',[100 100 figParams.sqSize figParams.sqSize]);
+set(gca,'FontName',figParams.fontName,'FontSize',figParams.axisFontSize,'LineWidth',figParams.axisLineWidth);
+plot3(XYZ1(1),XYZ1(2),XYZ1(3),'o','Color',[1 0 0],'MarkerFaceColor',[1 0 0],'MarkerSize',20);
+plot3(XYZ2(1),XYZ2(2),XYZ2(3),'o','Color',[0 0 1],'MarkerFaceColor',[0 0 1],'MarkerSize',20);
+view(30, 15);
+xlim([0 100]);
+ylim([0 100]);
+zlim([0 100]);
+xlabel('X','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
+ylabel('Y','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
+%set(gca,'XTickLabel',figParams.yTickLabels);
+set(gca,'YTickLabel',figParams.yTickLabels);
+set(gca,'ZTickLabel',figParams.yTickLabels);
+zlabel('Z','FontName',figParams.fontName,'FontSize',figParams.labelFontSize);
+axis('square');
+grid('on');
+ax = gca; 
+ax.YAxisLocation = 'origin';
 
 
 end
